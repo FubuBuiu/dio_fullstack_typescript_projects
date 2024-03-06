445648501bacb1d1cf561ca6e788284ac5eeb44a
Brendon Angelo <brendonangelo@hotmail.com>
2024-03-06 14:36:34 -0300

    chore: Body added in makeMockRequest

--- node_project/tests/__mocks__/mockRequest.mock.ts
+++ node_project/tests/__mocks__/mockRequest.mock.ts
@@ -1,8 +1,9 @@
 import { Request } from 'express';
 import { Params } from 'express-serve-static-core';
 
-export const makeMockRequest = ({ params, query, headers }: { params?: Params; query?: Params; headers?: Params }): Request => {
+export const makeMockRequest = ({ params, query, headers, body }: { params?: Params; query?: Params; headers?: Params; body?: Object }): Request => {
     const request = {
+        body: body || {},
         params: params || {},
         query: query || {},
         headers: headers || {},
