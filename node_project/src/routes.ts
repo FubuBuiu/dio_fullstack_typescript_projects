9188fa65e99006dbfd1c7969f8b9f58a3ad27c79
Brendon Angelo <brendonangelo@hotmail.com>
2024-03-06 14:43:12 -0300

    chore: New routes to bank account created

--- node_project/src/routes.ts
+++ node_project/src/routes.ts
@@ -2,12 +2,17 @@ import { Router } from "express";
 import { UserController } from './controllers/UserController';
 import { LoginController } from "./controllers/LoginController";
 import { verifyAuth } from "./midleware/verifyAuth";
+import { BankAccountController } from "./controllers/BankAccountController";
 
 export const router = Router();
 
 const userController = new UserController();
-
 const loginController = new LoginController();
+const bankAccountController = new BankAccountController();
+
+//TODO Adicionar validação de token para end points importantes
+
+//----USER----
 
 router.get('/users', userController.getAllUsers);
 
@@ -15,6 +20,20 @@ router.get('/users/:id?', verifyAuth, userController.getUser);
 
 router.delete('/users/:id?', userController.deleteUser);
 
+router.put('/users/:id?', userController.updateUser);
+
 router.post('/users', userController.createUser);
 
 router.post('/login', loginController.login);
+
+//----BANK ACCOUNT----
+
+router.get('/bankAccounts/account/:id?', bankAccountController.getBankAccountById);
+
+router.get('/bankAccounts/account/:currentAccount?/:agency?', bankAccountController.getBankAccountByCurrentAccountAndAgency);
+
+router.post('/bankAccounts/transfer', bankAccountController.makeTranser);
+
+router.post('/bankAccounts/pix/create', bankAccountController.createPixKey);
+
+router.get('/bankAccounts', bankAccountController.getAllBankAccounts);
