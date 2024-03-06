20e6b05222f06d7d36a41674160fd515e19bb900
Brendon Angelo <brendonangelo@hotmail.com>
2024-03-06 03:31:42 -0300

    chore: Added new properties to the user entity

--- node_project/src/entities/User.ts
+++ node_project/src/entities/User.ts
@@ -4,11 +4,22 @@ export class User {
     id: string;
     name: string;
     email: string;
+    phone?: number;
     password: string;
+    cpf: string;
+    adress?: {
+        street: string;
+        neighborhood: string;
+        city: string;
+        state: string;
+        zipCode: string;
+        complement?: string;
+    };
 
-    constructor(name: string, email: string, password: string) {
+    constructor(name: string, cpf: string, email: string, password: string) {
         this.id = randomUUID();
         this.name = name;
+        this.cpf = cpf;
         this.email = email;
         this.password = password;
     }
